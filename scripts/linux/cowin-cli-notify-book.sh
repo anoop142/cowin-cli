fadbd0f1675c66bfc4df85a095529e45ed25d9b3
anoop142 <anoop.skumar1507@gmail.com>
2021-05-12 09:05:06 +0530

    script: fix linux shell script * fix typo * use a single script * remove log function

--- scripts/linux/cowin-cli-notify-book.sh
+++ scripts/linux/cowin-cli-notify-book.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 # Anoop S
 # shell script to notify and book using cowin-cli
-# uses script book.sh for booking
+# Install notify-send for notifications
+
+
+COWIN_CLI="./cowin-cli"
 
 # Interval in seconds
 T=15
 # centers for grep matching
-CENTERS='center1|center2|cnter3'
-
+CENTERS_MATCH='center1|center2|cnter3'
+# centers to auto select
+CENTERS=""
 DISTRICT="district"
 STATE="state"
+AGE=45
+NAME=""
+NO=""
+
+
+
+
+
 
 notify(){
 	notify-send -u critical -t 6000 "Center found!"&
 	paplay /usr/share/sounds/freedesktop/stereo/complete.oga&
-	echo "$1" $(date) >>  log.txt
 }
 
 schedule(){
-	./book.sh
+	"$COWIN_CLI" -s "$STATE" -d "$DISTRICT" -sc -no "$NO" -name "$NAME" -centers "$CENTERS" && exit 0 
 }
 
 
 while :
 do
-	data=$(./cowin-cli -s $STATE  -d $DISTRICT -b  | grep -iE "$CENTERS" )
-	(( $? == 0  )) && echo "$data" && notify "$data" &&  schedule
-	sleep $t
+	echo "looking for centers.."
+
+	"$COWIN_CLI" -s "$STATE"  -d "$DISTRICT" -m "$AGE" -b
+
+	if (( $? == 0  )) 
+	then
+		notify
+		schedule
+	fi
+
+	sleep $T
 done
