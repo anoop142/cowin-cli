c682b56cac34d596101355384d08b7d393f4c6e5
Ishaan Rawat <ishaan@ishaanrawat.com>
2021-05-13 14:12:50 +0530

    notification script for mac os

---
+++ scripts/macos/cowin-cli-notify-book.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Anoop S
+# shell script to notify and book using cowin-cli
+# Install notify-send for notifications
+
+
+COWIN_CLI="./cowin-cli"
+
+# Interval in seconds
+T=15
+# centers for grep matching
+CENTERS_MATCH='center1|center2|cnter3'
+# centers to auto select
+CENTERS=""
+DISTRICT="district"
+STATE="state"
+AGE=45
+NAME=""
+NO=""
+
+
+
+
+
+
+notify(){
+	osascript -e 'display notification "Center found!" with title "COWIN Update" sound name "Submarine"'
+}
+
+schedule(){
+	"$COWIN_CLI" -s "$STATE" -d "$DISTRICT" -sc -no "$NO" -name "$NAME" -centers "$CENTERS" && exit 0 
+}
+
+
+while :
+do
+	echo "looking for centers.."
+
+	"$COWIN_CLI" -s "$STATE"  -d "$DISTRICT" -m "$AGE" -b
+
+	if (( $? == 0  )) 
+	then
+		notify
+		schedule
+	fi
+
+	sleep $T
+done
