4605ca96927656ace6b8cc91054e6d5d4b162603
anoop142 <anoop.skumar1507@gmail.com>
2021-05-25 22:36:27 +0530

    Add all cowin headers * to prevent breaking caused by the next cowin API's shenanigans

--- cowin/requests.go
+++ cowin/requests.go
@@ -9,9 +9,14 @@ import (
 )
 
 func reqAddHeaders(req *http.Request) {
-	req.Header.Add("user-agent", "Mozilla/5.0 (Linux x86_64) Chrome/90.0.4430.93 Safari/537.36")
+	req.Header.Add("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
 	req.Header.Add("origin", "https://selfregistration.cowin.gov.in/")
 	req.Header.Add("referer", "https://selfregistration.cowin.gov.in/")
+	req.Header.Add("sec-fetch-site", "cross-site")
+	req.Header.Add("sec-fetch-mode", "cors")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua", `" Not A;Brand";v="99", "Chromium";v="90", "Google Chrome";v="90"`)
+
 }
 
 func getReqAuth(URL, bearerToken string, auth bool) ([]byte, int) {
