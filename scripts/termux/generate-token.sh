e606428e6076c83a38e47490e300e60e2dc08951
anoop142 <anoop.skumar1507@gmail.com>
2021-06-03 17:37:13 +0530

    add token generation scripts

---
+++ scripts/termux/generate-token.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+COWIN_CLI="cowin-cli"
+# mobile no
+NO=""
+
+"$COWIN_CLI" -gen -no "$NO"
+
+
---
+++ scripts/windows/generate-token.bat
@@ -0,0 +1,13 @@
+:: Auto covid vaccine booking script using cowin-cli
+:: Anoop S
+
+@echo off
+
+set COWIN-CLI=.\cowin-cli.exe
+:: mobile number
+set NO=""
+
+
+%COWIN-CLI% -gen -no %NO%
+exit
+
